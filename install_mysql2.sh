b324ef6604e2d957f7cde4292acc11d8210f7755
BAlter <barnaby.alter@nyu.edu>
2019-04-01 15:26:28 -0400

    Add another commented out line to isntall mysql2 script

--- install_mysql2.sh
+++ install_mysql2.sh
@@ -3,3 +3,6 @@
 VERSION=$1
 
 env ARCHFLAGS="-arch x86_64" gem install mysql2 -v $VERSION -- --with-mysql-config=/usr/local/Cellar/mariadb-connector-c/2.2.2/bin/mariadb_config  --with-ldflags=-L/usr/local/opt/openssl/lib --with-cppflags=-I/usr/local/opt/openssl/include
+
+# OR
+# gem install mysql2 -v $VERSION -- --with-ldflags=-L/usr/local/opt/openssl/lib --with-cppflags=-I/usr/local/opt/openssl/include
